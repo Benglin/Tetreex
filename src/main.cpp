e57c23f866acc412934d0101f6920ac8e4cd1986
Ben Goh <benglin@outlook.com>
2015-06-01 15:14:36 +0800

    Adding Xcode project for Tetreex

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tetreex
+//
+//  Created by BEN GOH on 6/1/15.
+//  Copyright (c) 2015 BEN GOH. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
