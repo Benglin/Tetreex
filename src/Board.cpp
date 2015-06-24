9c12150dae8b984ffde74f58234d48b228aeccc1
Ben Goh <benglin@outlook.com>
2015-06-24 22:46:30 +0800

    Implemented Board::RefreshRegion API

--- src/Board.cpp
+++ src/Board.cpp
@@ -55,4 +55,35 @@ void Board::AddTetromino(Tetromino* pTetromino)
 
 void Board::RefreshRegion(int x, int y, int width, int height) const
 {
+    if (x >= mWidth || (y >= mHeight))
+        return; // Out of bound.
+
+    if (x < 0) {
+        width = width + x;
+        x = 0;
+    }
+
+    if (y < 0) {
+        height = height + y;
+        y = 0;
+    }
+
+    if (x + width > mWidth)
+        width = mWidth - x;
+    if (y + height > mHeight)
+        height = mHeight - y;
+
+    for (auto h = 0; h < height; h++)
+    {
+        auto pData = ContentAt(x, y + h);
+        for (auto w = 0; w < width; w++)
+        {
+            auto color = *(pData + w);
+
+            auto red   = ((color & 0x000000ff) >>  0);
+            auto green = ((color & 0x0000ff00) >>  8);
+            auto blue  = ((color & 0x00ff0000) >> 16);
+            mpCanvas->SetPixel(x + w, y + h, red, green, blue);
+        }
+    }
 }
